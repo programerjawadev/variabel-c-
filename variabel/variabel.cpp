42a5d2c95f15305cb07fa0ab8c227f3793a2770b
programerjawadev <programerjawa@gmail.com>
2025-07-28 06:46:17 +0700

    variabel c++

---
+++ variabel/variabel.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    a = 12;
+    cout << a << endl;
+
+    cout << "masukan nilai b : ";
+    int b;
+    cin >> b;
+    cout << "ini adalah nilai b yang anda masukan ";
+    cout << b << endl;
+}
